a7fec62de57c7a097e66d7dbe5d1bf5c9c7c4c53
Jordan Kremer <jkrem@pdx.edu>
2020-09-25 18:16:18 -0700

    Add BmpHeaderInfo constructor from rawdata

--- ImageTransformer/BmpHeaderInfo.cpp
+++ ImageTransformer/BmpHeaderInfo.cpp
@@ -8,17 +8,27 @@ BmpHeaderInfo::BmpHeaderInfo(const std::vector<unsigned char>& data)
 {
 	//bmpHeaderComponents->_ID = 
 	BmpConstants bmpConstants;
-	int loadIdx = 0;
-	uint32_t tmpCharToIntConversion;
 
-	for (int dataIdx = bmpConstants.FILESIZE; dataIdx < bmpConstants.RESERVED1; ++dataIdx) 
+	bmpHeaderComponents->_filesize = headerComponentsConstructorHelper(bmpConstants.FILESIZE, bmpConstants.RESERVED1, data);
+	bmpHeaderComponents->_width = headerComponentsConstructorHelper(bmpConstants.WIDTH, bmpConstants.HEIGHT, data);
+	bmpHeaderComponents->_height = headerComponentsConstructorHelper(bmpConstants.HEIGHT, bmpConstants.COLORPANES, data);
+	bmpHeaderComponents->_compression = headerComponentsConstructorHelper(bmpConstants.COMPRESSION, bmpConstants.IMAGESIZE, data);
+	bmpHeaderComponents->_horizontalResolution = headerComponentsConstructorHelper(bmpConstants.HORIZONTALRESOLUTION, bmpConstants.VERTICALRESOLUTION, data);
+	bmpHeaderComponents->_verticalResolution = headerComponentsConstructorHelper(bmpConstants.VERTICALRESOLUTION, bmpConstants.NUMCOLORSINPALETTE, data);
+
+}
+
+uint32_t BmpHeaderInfo::headerComponentsConstructorHelper(const int bmpConstantStart, const int bmpConstantEnd, const std::vector<unsigned char>& data)
+{
+	uint32_t tmpCharToIntConversion;
+	int loadIdx = 0;
+	for (int dataIdx = bmpConstantStart; dataIdx < bmpConstantEnd; ++dataIdx)
 	{
 		((unsigned char*)& tmpCharToIntConversion)[loadIdx] = data[dataIdx];
+		++loadIdx;
 	}
-	bmpHeaderComponents->_filesize = tmpCharToIntConversion;
 
-
-
+	return tmpCharToIntConversion;
 }
 
 
