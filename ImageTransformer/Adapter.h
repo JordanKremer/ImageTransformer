7f46cd7fe9e3e18994f5bf59e75a7c8e7c176de7
Jordan Kremer <jkrem@pdx.edu>
2020-11-25 17:19:01 -0800

    Add comments

--- ImageTransformer/Adapter.h
+++ ImageTransformer/Adapter.h
@@ -1,3 +1,12 @@
+/*
+Author : Jordan Kremer
+11/20/2020
+Adapter.h
+
+Base class for Adapters, includes functionality to adapt to and from rawData and Data objects.
+Used to illustrate the adapter pattern.
+*/
+
 #pragma once
 #include "Data.h"
 #include <vector>
@@ -5,7 +14,7 @@
 class Adapter
 {
 public:
-	virtual std::unique_ptr<Data> AdaptFromRaw(std::vector<unsigned char>& data) = 0;
+	virtual std::unique_ptr<Data> AdaptFromRaw(std::vector<unsigned char>& rawData) = 0;
 	virtual std::vector<unsigned char> AdapterToRaw(std::unique_ptr<Data> data) = 0;
 };
 
