0a0fd10b3e3551cecb24f6186bbd050863b32d3a
Jordan Kremer <jkrem@pdx.edu>
2020-12-15 12:40:12 -0800

    Change return type to unique_ptr, add comments

--- ImageTransformer/Transformation.h
+++ ImageTransformer/Transformation.h
@@ -3,7 +3,7 @@ Author : Jordan Kremer
 11/20/20
 Transformation.h
 
-This class is an abstract base class wherein Transformation subclasses such
+This class is an interface class wherein Transformation subclasses such
 as Gaussian blur and Pixelate will derive from.
 */
 
@@ -13,6 +13,8 @@ as Gaussian blur and Pixelate will derive from.
 class Transformation
 {
 public:
-	virtual Data Transform(Data imageData) = 0;
+	//Unique_ptr obj is destroyed within the function and a new one is created
+	//with the transformed Data, as a new Data obj
+	virtual std::unique_ptr<Data> Transform(std::unique_ptr<Data> adaptedData) = 0;
 };
 
