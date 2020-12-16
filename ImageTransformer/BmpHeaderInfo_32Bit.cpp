16f5bbe8e5f37d51ea1b1004ab9cff1c8ddb9848
Jordan Kremer <jkrem@pdx.edu>
2020-12-15 16:53:44 -0800

    Add initializaton for bmpHeaderComponents_32Bit variables, add getter implementation

--- ImageTransformer/BmpHeaderInfo_32Bit.cpp
+++ ImageTransformer/BmpHeaderInfo_32Bit.cpp
@@ -10,7 +10,13 @@ BmpHeaderInfo_32Bit::BmpHeaderInfo_32Bit(BmpHeaderInfo_32Bit& to_copy)
 
 BmpHeaderInfo_32Bit::BmpHeaderInfo_32Bit(std::vector<unsigned char>& hData):BmpHeaderInfo(hData)
 {
+	BmpHeader_32BitConstants bmpConstants_32Bit;
 
+	bmpHeaderComponents_32Bit = std::move(std::make_unique<BmpHeaderComponents_32Bit>());
+	bmpHeaderComponents_32Bit->_redBitMask = headerComponentsConstructorHelper(bmpConstants_32Bit.RED_CHANNEL_BITMASK, bmpConstants_32Bit.GREEN_CHANNEL_BITMASK, hData);
+	bmpHeaderComponents_32Bit->_greenBitmask = headerComponentsConstructorHelper(bmpConstants_32Bit.GREEN_CHANNEL_BITMASK, bmpConstants_32Bit.BLUE_CHANNEL_BITMASK, hData);
+	bmpHeaderComponents_32Bit->_blueBitMask = headerComponentsConstructorHelper(bmpConstants_32Bit.BLUE_CHANNEL_BITMASK, bmpConstants_32Bit.ALPHA_CHANNEL_BITMASK, hData);
+	bmpHeaderComponents_32Bit->_alphaBitMask = headerComponentsConstructorHelper(bmpConstants_32Bit.ALPHA_CHANNEL_BITMASK, bmpConstants_32Bit.ALPHA_CHANNEL_BITMASK + 4, hData);
 }
 
 HeaderInfo* BmpHeaderInfo_32Bit::Clone() const
@@ -20,6 +26,26 @@ HeaderInfo* BmpHeaderInfo_32Bit::Clone() const
 
 void BmpHeaderInfo_32Bit::operator=(const HeaderInfo& toCopy)
 {
+}
+
+const uint32_t BmpHeaderInfo_32Bit::GetRedBitMask()
+{
+	return bmpHeaderComponents_32Bit->_redBitMask;
+}
+
+const uint32_t BmpHeaderInfo_32Bit::GetGreenBitMask()
+{
+	return bmpHeaderComponents_32Bit->_greenBitmask;
+}
+
+const uint32_t BmpHeaderInfo_32Bit::GetBlueBitMask()
+{
+	return bmpHeaderComponents_32Bit->_blueBitMask;
+}
+
+const uint32_t BmpHeaderInfo_32Bit::GetAlphaBitMask()
+{
+	return bmpHeaderComponents_32Bit->_alphaBitMask;
 }
 
 
