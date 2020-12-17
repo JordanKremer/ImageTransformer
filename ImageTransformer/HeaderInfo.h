21912f1436616c647fe94c55d03bd44c1a3c234b
Jordan Kremer <jkrem@pdx.edu>
2020-12-16 17:58:59 -0800

    Add base class constructors, remove ref & from GetRawHeader()

--- ImageTransformer/HeaderInfo.h
+++ ImageTransformer/HeaderInfo.h
@@ -16,10 +16,33 @@ class HeaderInfo
 {
 public:
 	//https://isocpp.org/wiki/faq/abcs#copy-of-abc-via-clone
-	virtual HeaderInfo* Clone() const = 0;
+	HeaderInfo(uint32_t width, uint32_t height, std::unique_ptr<HeaderInfo> to_copy);
+	HeaderInfo(uint32_t width, uint32_t height, uint32_t fileSize, std::unique_ptr<HeaderInfo> to_copy);
+
+	_NODISCARD virtual std::unique_ptr<HeaderInfo> Clone() const = 0;
 	_NODISCARD virtual const uint32_t GetCompression() const = 0;
 	_NODISCARD virtual const uint32_t GetBitsPerPixel() const = 0;
 	_NODISCARD virtual const uint32_t GetImageStartOffset() const = 0;
 	_NODISCARD virtual const uint32_t GetWidth() const = 0;
-	_NODISCARD virtual const std::vector<unsigned char>& GetRawHeader() const = 0;
+	_NODISCARD virtual const std::vector<unsigned char> GetRawHeader() const = 0;
 };
+
+/*
+
+
+Various Header constructors?
+	Header(new width, new height, header* to_copy)
+	header(new width, new height, new filesize, header* to_copy)
+		:: IMPORTANT, the new filesize is only for the scaling up or down
+
+	these are virtual functions, which will dynamically dispatch to
+	the correct subclasses to deal with this
+
+	that way a JPEG can deal with jpeg construction, bmp can deal
+	with bmp, etc 
+	
+
+	This will still keep it abstract so that any type can be used
+	with the current setup as long as we are using Data as a
+	generic
+	*/
