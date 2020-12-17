965599dac64e09eeaccf8bc6aba9a67f65a6b9ee
Jordan Kremer <jkrem@pdx.edu>
2020-12-16 18:02:56 -0800

    Remove const from Clone()

--- ImageTransformer/HeaderInfo.h
+++ ImageTransformer/HeaderInfo.h
@@ -19,7 +19,7 @@ public:
 	HeaderInfo(uint32_t width, uint32_t height, std::unique_ptr<HeaderInfo> to_copy);
 	HeaderInfo(uint32_t width, uint32_t height, uint32_t fileSize, std::unique_ptr<HeaderInfo> to_copy);
 
-	_NODISCARD virtual std::unique_ptr<HeaderInfo> Clone() const = 0;
+	_NODISCARD virtual std::unique_ptr<HeaderInfo> Clone() = 0;
 	_NODISCARD virtual const uint32_t GetCompression() const = 0;
 	_NODISCARD virtual const uint32_t GetBitsPerPixel() const = 0;
 	_NODISCARD virtual const uint32_t GetImageStartOffset() const = 0;
