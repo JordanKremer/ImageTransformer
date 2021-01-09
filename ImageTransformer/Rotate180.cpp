ec8898688a057e915bd284e5f330f70c6ae40cc2
Jordan Kremer <jkrem@pdx.edu>
2021-01-09 14:24:26 -0800

    Change variable names for Swap function, change algorithm for Transformation, needs to be tested

--- ImageTransformer/Rotate180.cpp
+++ ImageTransformer/Rotate180.cpp
@@ -1,74 +1,55 @@
 #include "Rotate180.h"
 
-/*
-std::unique_ptr<Data> Rotate180::Transform(std::unique_ptr<Data> adaptedData)
-{
-	
-	vector<unsigned char>tmpVec;
-	bool hasCountedWidth = false;
-	for (int y = (Height - 1); y > -1; y--)
-	{
-		for (int x = (Width - 1); x > -1; x--)
-		{
-			pushPixelOntoVector(tmpVec, x, y);
-			if (!hasCountedWidth)
-				++scaledWidth;
-		}
-		hasCountedWidth = true;
-		++scaledHeight;
-	}
-	pixelByte = tmpVec;
-	
-
-	adaptedData->_header;
-
-
-	//data needs args for constructor
-	//return std::move(std::make_unique<Data>());
-	return nullptr;
-}
-*/
 
 
 std::vector<Pixel> Rotate180::TransformPixels(std::vector<Pixel> pixels, const HeaderInfo* hdr)
 {
 	uint32_t imageWidth = hdr->GetWidth();
 	uint32_t imageHeight = hdr->GetHeight();
-	int curLowerBoundIdx = 0;
-	int curUpperBoundIdx = imageHeight;
+	uint32_t pixelIdxToSwap;
+	int curWidth;
+	int curHeight;
+	int mirrorHeight;
+	int mirrorWidth;
 
-	for (int widthIdx = 0; widthIdx < imageWidth; ++widthIdx)
+
+	if (imageHeight * imageWidth > pixels.size())
+		throw std::runtime_error("ERROR: INVALID IMAGE DIMENSIONS, CANNOT TRANSFORM");
+
+	for (uint32_t curPixelIdx = 0; curPixelIdx < pixels.size(); ++curPixelIdx)
 	{
-		curUpperBoundIdx = imageHeight;
-		for (int heightIdx = 0; heightIdx < imageHeight / 2; ++heightIdx)
-		{
-			curLowerBoundIdx = widthIdx * heightIdx;
-			
-			try {
-				Swap(pixels, curUpperBoundIdx, curLowerBoundIdx);
-			}
-			catch (const std::out_of_range& oor)
-			{
-				//either throw to next level up, or just output a message here
-			}
+		curWidth = curPixelIdx % imageWidth;
+		curHeight = curPixelIdx / imageHeight;
 
-			--curUpperBoundIdx;
+		mirrorWidth = imageWidth - curWidth;
+		mirrorHeight = imageHeight - curHeight;
+		pixelIdxToSwap = mirrorHeight * imageWidth + mirrorWidth;
+		
+		try {
+			Swap(pixels, pixelIdxToSwap, curPixelIdx);
 		}
-	}
+		catch (const std::out_of_range& oor)
+		{
+			//either throw to next level up, or just output a message here
+			throw oor;
+		}
 
+	}
 
 	return pixels;
 }
+
+
 
 //not sure if I should be passing by ref or just using the move copy...it might be 
 //really slow to execute it a lot
 //pass by ref?
 //dont make a function at all?
-void Rotate180::Swap(std::vector<Pixel>& pixels, int curUpperBoundIdx, int curLowerBoundIdx)
+void Rotate180::Swap(std::vector<Pixel>& pixels, uint32_t pixelIdxToSwap, int curPixelIdx)
 {
-	Pixel tmp = pixels[curLowerBoundIdx];
-	pixels[curLowerBoundIdx] = pixels[curUpperBoundIdx];
-	pixels[curUpperBoundIdx] = tmp;
+	Pixel tmp = pixels[curPixelIdx];
+	pixels[curPixelIdx] = pixels[pixelIdxToSwap];
+	pixels[pixelIdxToSwap] = tmp;
 }
 
 
@@ -77,3 +58,6 @@ std::unique_ptr<HeaderInfo> Rotate180::TransformHeader(std::unique_ptr<HeaderInf
 	//No op function
 	return std::move(header);
 }
+
+
+
--- ImageTransformer/Rotate180.h
+++ ImageTransformer/Rotate180.h
@@ -5,11 +5,10 @@ class Rotate180 :
 	public Transformation
 {
 public:
-	Rotate180();
 	std::vector<Pixel> TransformPixels(std::vector<Pixel> pixels, const HeaderInfo* hdr);
 	std::unique_ptr<HeaderInfo> TransformHeader(std::unique_ptr<HeaderInfo> header);
 private:
-	void Swap(std::vector<Pixel>& pixels, int curUpperBoundIdx, int curLowerBoundIdx);
+	void Swap(std::vector<Pixel>& pixels, uint32_t pixelIdxToSwap, int curPixelIdx);
 
 };
 
