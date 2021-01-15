333fa9066d4aa685f96a6a275c3b7ad740d53236
Jkrem06 <44076055+Jkrem06@users.noreply.github.com>
2021-01-14 16:01:20 -0800

    Add squareLen as private variable

--- ImageTransformer/Pixelate.cpp
+++ ImageTransformer/Pixelate.cpp
@@ -2,15 +2,182 @@
 
 Pixelate::Pixelate()
 {
+	squareLen = 16;
 }
+
+
 
 std::vector<Pixel> Pixelate::TransformPixels(std::vector<Pixel> pixels, const HeaderInfo* hdr)
 {
-	return std::vector<Pixel>();
+	//return error if the image is not atleast 16x16?
+
+
+	/*
+	uint8_t xFull16Additions = Width / 16;			//Given a 16x16 block, how many full blocks are available for x
+	uint8_t yFull16Additions = Height / 16;			//Given a 16x16 block, how many full blocks are available for x
+	uint8_t xRemainder = Width % 16;				//Remainder values if 16x16 doesn't fit perfectly
+	uint8_t yRemainder = Height % 16;
+	uint8_t boundCheckerX = 0;
+	uint8_t boundCheckerY = 0;
+	uint8_t to_addY = 16;
+	uint8_t to_addX = 16;
+
+
+	if (xRemainder > yRemainder)
+	{
+		xRemainder = yRemainder;
+	}
+	else
+		yRemainder = xRemainder;
+
+	//Pixelates 16x16 blocks, + remainder blocks
+	for (uint32_t x = 0; x < (Width); x += to_addX)
+	{
+		for (uint32_t y = 0; y < (Height); y += to_addY)
+		{
+			if (xRemainder > 0)
+			{
+				if (boundCheckerY == (yFull16Additions))	//If we have reached the amount of full 16x16 blocks
+					to_addY = (yRemainder);					//then pixelate the remainder pixels
+			}
+			average16RGB(x, y, to_addX, to_addY);	//Averages the RGB values within given bounds, saves pixel data
+			boundCheckerY++;
+		}
+
+		boundCheckerY = 0;
+		to_addY = 16;
+		boundCheckerX++;
+
+		if (xRemainder > 0)
+		{
+			if (boundCheckerX == (xFull16Additions))
+				to_addX = (xRemainder);
+		}
+	}
+	*/
+	uint32_t Width = hdr->GetWidth();
+	uint32_t Height = hdr->GetHeight();
+
+	uint8_t xFull16Additions = Width / 16;			//Given a 16x16 block, how many full blocks are available for x
+	uint8_t yFull16Additions = Height / 16;			//Given a 16x16 block, how many full blocks are available for x
+	uint8_t xRemainder = Width % 16;				//Remainder values if 16x16 doesn't fit perfectly
+	uint8_t yRemainder = Height % 16;
+	uint8_t boundCheckerX = 0;
+	uint8_t boundCheckerY = 0;
+	uint8_t to_addY = 16;
+	uint8_t to_addX = 16;
+
+
+
+	if (xRemainder > yRemainder)
+	{
+		xRemainder = yRemainder;
+	}
+	else
+		yRemainder = xRemainder;
+
+	
+	//Pixelates 16x16 blocks, + remainder blocks
+	for (uint32_t x = 0; x < (Width); x += to_addX)
+	{
+		for (uint32_t y = 0; y < (Height); y += to_addY)
+		{
+			if (xRemainder > 0)
+			{
+				if (boundCheckerY == (yFull16Additions))	//If we have reached the amount of full 16x16 blocks
+					to_addY = (yRemainder);					//then pixelate the remainder pixels
+			}
+			average16RGB(x, y, to_addX, to_addY);	//Averages the RGB values within given bounds, saves pixel data
+			boundCheckerY++;
+		}
+
+		boundCheckerY = 0;
+		to_addY = 16;
+		boundCheckerX++;
+
+		if (xRemainder > 0)
+		{
+			if (boundCheckerX == (xFull16Additions))
+				to_addX = (xRemainder);
+		}
+	}
+
+	return pixels;
 }
+
+
+
+
+Pixel Pixelate::Average16RGB(const HeaderInfo* hdr, std::vector<Pixel> pixels, uint32_t x, uint32_t y, uint8_t xBoundAddition, uint8_t yBoundAddition) {
+	uint32_t tmpR = 0;
+	uint32_t tmpG = 0;
+	uint32_t tmpB = 0;
+
+
+	//build dummy tmp pixel
+	auto channelCount = pixels[0].GetChannelCount();
+	std::vector<unsigned char> channelData;
+	for (int i = 0; i < channelCount; ++i)
+	{
+		channelData.push_back(0);
+	}
+
+	Pixel tmpPixel(channelData, channelCount);
+
+
+
+	if (xBoundAddition == 0)
+		xBoundAddition = 16;
+	if (yBoundAddition == 0)
+		yBoundAddition = 16;
+
+
+	//Add pixel values together
+	for (uint32_t imageWidth = x; imageWidth < (x+xBoundAddition); imageWidth++)
+	{
+		for (uint32_t imageHeight = y; imageHeight < (y + yBoundAddition); imageHeight++)
+		{
+
+			tmpPixel = tmpPixel + GetPixelAtCoordinate(pixels, hdr, imageWidth, imageHeight);
+
+			/*
+			tmpR += returnRedByte(imageWidth, imageHeight);
+			tmpG += returnGreenByte(imageWidth, imageHeight);
+			tmpB += returnBlueByte(imageWidth, imageHeight);
+			*/
+		}
+	}
+
+
+	for (auto& channel : tmpPixel.GetAllChannelData())
+	{
+
+	}
+
+
+	tmpR = tmpR / (xBoundAddition * yBoundAddition);
+	tmpB = tmpB / (xBoundAddition * yBoundAddition);
+	tmpG = tmpG / (xBoundAddition * yBoundAddition);
+
+
+	//in a 16x 16 block, make all the pixels values the average that was just determined
+	for (uint32_t imageWidth = x; imageWidth <(x+xBoundAddition); imageWidth++)
+	{
+		for (uint32_t imageHeight = y; imageHeight < (y +yBoundAddition); imageHeight++)
+		{
+			returnRedByte(imageWidth, imageHeight) = tmpR;
+			returnGreenByte(imageWidth, imageHeight) = tmpG;
+			returnBlueByte(imageWidth, imageHeight) = tmpB;
+		}
+	}
+}
+
+
 
 std::unique_ptr<HeaderInfo> Pixelate::TransformHeader(std::unique_ptr<HeaderInfo> header)
 {
 	return std::unique_ptr<HeaderInfo>();
 }
+
+
 
--- ImageTransformer/Pixelate.h
+++ ImageTransformer/Pixelate.h
@@ -7,6 +7,9 @@ public:
 	Pixelate();
 	std::vector<Pixel> TransformPixels(std::vector<Pixel> pixels, const HeaderInfo* hdr);
 	std::unique_ptr<HeaderInfo> TransformHeader(std::unique_ptr<HeaderInfo> header);
+
+private:
+	Pixel Average16RGB(const HeaderInfo* hdr, std::vector<Pixel> pixels, uint32_t x, uint32_t y, uint8_t xBoundAddition, uint8_t yBoundAddition);
+	int squareLen;
+
 };
-
-
