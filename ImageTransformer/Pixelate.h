17f3dc5442ded7d14c88f852ea516c690857b8fc
Jkrem06 <44076055+Jkrem06@users.noreply.github.com>
2021-01-15 21:08:35 -0800

    Change function signatures and declarations to match changes in Transformation.h

--- ImageTransformer/Pixelate.h
+++ ImageTransformer/Pixelate.h
@@ -5,14 +5,13 @@ class Pixelate :
 {
 public:
 	Pixelate();
-	std::vector<Pixel> TransformPixels(std::vector<Pixel> pixels, const HeaderInfo* hdr);
-	std::unique_ptr<HeaderInfo> TransformHeader(std::unique_ptr<HeaderInfo> header);
+	std::vector<Pixel> TransformPixels(std::vector<Pixel> pixels);
+	std::unique_ptr<HeaderInfo> TransformHeader(std::unique_ptr<HeaderInfo> hdr);
 	
-protected:
-	std::vector<std::vector<Pixel>::iterator> GetIteratorsOfPixelBox(std::vector<Pixel> pixels, int curBox_X, int curBox_Y);
-	uint32_t GetBoxIteratorOffset(int curBox_X, int curBox_Y, int row, int width);
-
 private:
-	Pixel Average16RGB(const HeaderInfo* hdr, std::vector<Pixel> pixels, uint32_t x, uint32_t y, uint8_t xBoundAddition, uint8_t yBoundAddition);
+//	std::vector<std::vector<Pixel>::iterator> GetIteratorsOfPixelBox(std::vector<Pixel> pixels, int curBox_X, int curBox_Y);
+//	uint32_t GetBoxIteratorOffset(int curBox_X, int curBox_Y, int row, int width);
+
+	//Pixel Average16RGB(const HeaderInfo* hdr, std::vector<Pixel> pixels, uint32_t x, uint32_t y, uint8_t xBoundAddition, uint8_t yBoundAddition);
 	int squareLen;
 };
