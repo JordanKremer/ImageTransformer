6408283b54364c76bc133a318679a773be47ef35
Jkrem06 <44076055+Jkrem06@users.noreply.github.com>
2021-01-15 21:06:39 -0800

    Remove Swap(), modify functions to reflect changes to Transformation base class, including the usage of a private: const HeaderInfo*, and GetHeader()

--- ImageTransformer/Rotate180.cpp
+++ ImageTransformer/Rotate180.cpp
@@ -2,59 +2,28 @@
 
 
 
-std::vector<Pixel> Rotate180::TransformPixels(std::vector<Pixel> pixels, const HeaderInfo* hdr)
+std::vector<Pixel> Rotate180::TransformPixels(std::vector<Pixel> pixels)
 {
+	auto hdr = GetHeader();
+	
+	if (!hdr) {
+		throw std::runtime_error("ERROR | ROTATE180::TRANSFORMPIXELS(): FAILED TO SET HEADER");
+	}
+		
 	uint32_t imageWidth = hdr->GetWidth();
 	uint32_t imageHeight = hdr->GetHeight();
-	uint32_t pixelIdxToSwap;
-	int curWidth;
-	int curHeight;
-	int mirrorHeight;
-	int mirrorWidth;
-
 
 	if (imageHeight * imageWidth > pixels.size())
 		throw std::runtime_error("ERROR | ROTATE180.TRANSFORMPIXELS(): INVALID IMAGE DIMENSIONS, CANNOT TRANSFORM");
-
-	for (uint32_t curPixelIdx = 0; curPixelIdx < pixels.size(); ++curPixelIdx)
-	{
-		curWidth = curPixelIdx % imageWidth;
-		curHeight = curPixelIdx / imageHeight;
-
-		mirrorWidth = imageWidth - curWidth;
-		mirrorHeight = imageHeight - curHeight;
-		pixelIdxToSwap = mirrorHeight * imageWidth + mirrorWidth;
-		
-		try {
-			Swap(pixels, pixelIdxToSwap, curPixelIdx);
-		}
-		catch (const std::out_of_range& oor)
-		{
-			throw oor;
-		}
-
-	}
-
+	std::reverse(pixels.begin(), pixels.end());
 	return pixels;
 }
 
 
-
-void Rotate180::Swap(std::vector<Pixel>& pixels, uint32_t pixelIdxToSwap, int curPixelIdx)
-{
-	if (curPixelIdx > pixels.size() || pixelIdxToSwap > pixels.size())
-		throw std::out_of_range("ERROR | ROTATE180.SWAP(): SWAP PIXEL FAILURE");
-	
-	Pixel tmp = pixels[curPixelIdx];
-	pixels[curPixelIdx] = pixels[pixelIdxToSwap];
-	pixels[pixelIdxToSwap] = tmp;
-}
-
-
-std::unique_ptr<HeaderInfo> Rotate180::TransformHeader(std::unique_ptr<HeaderInfo> header)
+std::unique_ptr<HeaderInfo> Rotate180::TransformHeader(std::unique_ptr<HeaderInfo> hdr)
 {
 	//No op function
-	return std::move(header);
+	return std::move(hdr);
 }
 
 
--- ImageTransformer/Rotate180.h
+++ ImageTransformer/Rotate180.h
@@ -5,10 +5,8 @@ class Rotate180 :
 	public Transformation
 {
 public:
-	std::vector<Pixel> TransformPixels(std::vector<Pixel> pixels, const HeaderInfo* hdr);
-	std::unique_ptr<HeaderInfo> TransformHeader(std::unique_ptr<HeaderInfo> header);
-private:
-	void Swap(std::vector<Pixel>& pixels, uint32_t pixelIdxToSwap, int curPixelIdx);
+	std::vector<Pixel> TransformPixels(std::vector<Pixel> pixels);
+	std::unique_ptr<HeaderInfo> TransformHeader(std::unique_ptr<HeaderInfo>);
 
 };
 
