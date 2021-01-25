a3d81aa48bbc4012c421dcb5118b58c37483d425
Jkrem06 <jordan.kremer@gmail.com>
2021-01-24 21:46:24 -0800

    Test change user

--- ImageTransformer/adapter_factory.h
+++ ImageTransformer/adapter_factory.h
@@ -6,5 +6,6 @@ class adapter_factory
 public:
 	//Each supported filetype will need a different adapter to convert to generic_image
 	std::unique_ptr<Adapter> get_adapter(std::string file_type);
+//test
 };
 
